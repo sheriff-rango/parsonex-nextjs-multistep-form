c246622b9e222c5dbb92f2e668f314ad21da8751
Trevor Williams <tmw7991@gmail.com>
2025-02-18 09:36:33 -0700

    use session to check auth

--- app/dashboard/(admin)/layout.tsx
+++ app/dashboard/(admin)/layout.tsx
@@ -1,14 +1,7 @@
-import { redirect } from "next/navigation";
-import { checkAdmin } from "@/server/server-only/auth";
-
 export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  if (!(await checkAdmin())) {
-    redirect("/dashboard");
-  }
-
   return <>{children}</>;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { PrivateMetadata } from "@/types";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isAdminRoute = createRouteMatcher([
+  "/dashboard/admin(.*)",
+  "/dashboard/(admin)/(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+
+    if (isAdminRoute(req)) {
+      const { sessionClaims } = await auth();
+      const privateMetadata = sessionClaims?.privateMetadata as PrivateMetadata;
+      const isAdmin = privateMetadata?.role === "admin";
+      if (!isAdmin) {
+        return new Response("Unauthorized", { status: 401 });
+      }
+    }
+  }
 });
 
 export const config = {
